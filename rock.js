7544baa6c9272cec493784542c774050932b4d2e
Prajwal Nandeshwar <prajwaljn1008@gmail.com>
2021-06-04 14:58:25 +0530

    Add files via upload

---
+++ rock.js
@@ -0,0 +1,54 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const userScoreSpan = document.querySelector('[data-user-score]')
+const SELECTIONS = [
+{
+name: 'rock',
+emoji: '👊',
+beats: 'scissors'
+},
+{
+name: 'paper',
+emoji: '✋',
+beats: 'rock'
+},
+{
+name: 'scissors',
+emoji: '✌',
+beats: 'paper'
+}
+]
+selectionButtons.forEach(selectionButton => {
+selectionButton.addEventListener('click', e => {
+const selectionName = selectionButton.dataset.selection
+const selection = SELECTIONS.find(selection => selection.name === selectionName)
+makeSelection(selection)
+})
+})
+function makeSelection(selection) {
+const computerSelection = randomSelection()
+const userWinner = isWinner(selection, computerSelection)
+const computerWinner = isWinner(computerSelection, selection)
+addSelectionResult(computerSelection, computerWinner)
+addSelectionResult(selection, userWinner)
+if (userWinner) incrementScore(userScoreSpan)
+if (computerWinner) incrementScore(computerScoreSpan)
+}
+function incrementScore(scoreSpan) {
+scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+function addSelectionResult(selection, winner) {
+const div = document.createElement('div')
+div.innerText = selection.emoji
+div.classList.add('result-selection')
+if (winner) div.classList.add('winner')
+finalColumn.after(div)
+}
+function isWinner(selection, opponentSelection) {
+return selection.beats === opponentSelection.name
+}
+function randomSelection() {
+const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+return SELECTIONS[randomIndex]
+}
